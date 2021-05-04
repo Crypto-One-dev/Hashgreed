ea408368fd499069360662c342a600861b190031
Shing Ho Tan <talenteyedev@gmail.com>
2021-05-04 04:40:49 -0400

    Add StartOver button logic in Index Page

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import cx from 'classnames'
+import {useHistory} from 'react-router-dom'
 
 import Steps from './components/Steps'
 import styles from './Main.module.scss'
 
 import walletContainer from 'redux/containers/wallet'
+import WavesUtils from 'utils/waves'
 
 function Main({walletState, walletActions}) {
+  const history = useHistory()
+
+  const sign = () => {
+    WavesUtils.unlockWallet('CLOUD', walletActions.unlockWallet, walletActions.lockWallet)
+  }
+  const gotoOverview = useCallback(() => history.push('/overview'), [history])
   return (
     <div className={styles.main}>
       <div className={styles.header}>
@@ -19,7 +27,7 @@ function Main({walletState, walletActions}) {
         To import an existing account, add it first in waves.exchange
       </div> 
       <div className={styles.startnow}>
-        <a className={cx(styles.start, styles.filled)}>Start now</a>
+        <a className={cx(styles.start, styles.filled)} onClick={walletState.address ? gotoOverview : sign}>Start now</a>
       </div>
       <Steps />
     </div>
